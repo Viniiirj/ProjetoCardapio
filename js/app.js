732c8ac67db8bb8059a6d62732fb0cc8039ac1cf
viniiirj <vsr.rio@gmail.com>
2023-09-14 17:23:15 -0300

    Correção de bug no envio de pedidos pelo zap

--- js/app.js
+++ js/app.js
@@ -439,36 +439,30 @@ cardapio.metodos = {
 
         // Atualualiza o link do whatsapp
         finalizarPedido: () => {
-            
-            if(MEU_CARRINHO.length > 0 && MEU_ENDERECO != null) {
-                
-                
-                var texto = `Olá gostaria de fazer um pedido:`;
-                texto += `\n*Itens do pedido:*\n\n${itens}`;
-                texto += `\n*Endereço de entrega:*`;
-                texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`
-                texto += `\n${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf}/${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`
-                texto += `\n\n*Total (com entrega): R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.',',')}*`;
-                
+            if (MEU_CARRINHO.length > 0 && MEU_ENDERECO != null) {
                 var itens = '';
-                
-                $.each(MEU_CARRINHO, (i,e) => {
-                    itens += `*${e.qntd}x* ${e.name} ....... R$ ${e.price.toFixed(2).replace('.',',')} \n`
-                    
-                    if((i + 1) == MEU_CARRINHO.length) {
-                        
-                        texto = texto.replace(/\${itens}/g, itens);
-                        
+        
+                $.each(MEU_CARRINHO, (i, e) => {
+                    itens += `*${e.qntd}x* ${e.name} ....... R$ ${e.price.toFixed(2).replace('.', ',')} \n`;
+        
+                    if ((i + 1) == MEU_CARRINHO.length) {
+                        var texto = `Olá gostaria de fazer um pedido:`;
+                        texto += `\n*Itens do pedido:*\n\n${itens}`;
+                        texto += `\n*Endereço de entrega:*`;
+                        texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`
+                        texto += `\n${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf}/${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`
+                        texto += `\n\n*Total (com entrega): R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}*`;
+        
                         //converte a URL
                         let encode = encodeURI(texto);
                         let Url = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
-                        
+        
                         $('#btnEtapaResumo').attr('href', Url);
-                        
                     }
-                })
+                });
             }
         },
+        
 
         // carrega o link do botão reserva
         carregarBotaoReserva: () => {
